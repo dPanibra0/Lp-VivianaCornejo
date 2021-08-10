f6407e753d4cd7cb678f2d808d47b214b2837de0
David Panibra <dpanibra0@gmail.com>
2021-08-10 17:28:58 -0500

    to firefox

--- main.js
+++ main.js
@@ -33,7 +33,6 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -43,11 +42,7 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
 }
 
 function currentSlide(n) {
